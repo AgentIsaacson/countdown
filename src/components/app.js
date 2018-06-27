b0ca301817a3a14e3fbe6754eb852f6d6dce4da6
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-27 16:04:31 -0600

    added birthday countdown title

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className="grid">
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
-        <div className="grid__skew-light-one"></div>
-        <div className="grid__skew-light-two"></div>
-        <div className="grid__skew-light-three-box"></div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="grid">
+            <h1 className="grid__title">Birthday Countdown</h1>
+                <div className="grid__skew-dark-blue" />
+                <div className="grid__skew-light-blue" />
+            </div>
+        );
+    }
 }
