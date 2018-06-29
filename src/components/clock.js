fffba3ace01fa1dd593f9a3cd93474373e66b4db
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-29 11:41:41 -0600

    style fixes

--- src/components/clock.js
+++ src/components/clock.js
@@ -5,28 +5,20 @@ export default class Clock extends Component {
 		return (
 			<div className="clock">
 				<div className="clock__days clock__box">
-					<div>
-						<label className="clock__title">DAYS</label>
-						<label className="clock__amount">300</label>
-					</div>
+					<label className="clock__title">DAYS</label>
+					<label className="clock__amount">300</label>
 				</div>
 				<div className="clock__hours clock__box">
-					<div>
-						<label className="clock__title">HOURS</label>
-						<label className="clock__amount">16</label>
-					</div>
+					<label className="clock__title">HOURS</label>
+					<label className="clock__amount">16</label>
 				</div>
 				<div className="clock__minutes clock__box">
-					<div>
-						<label className="clock__title">MINUTES</label>
-						<label className="clock__amount">42</label>
-					</div>
+					<label className="clock__title">MINUTES</label>
+					<label className="clock__amount">42</label>
 				</div>
 				<div className="clock__seconds clock__box">
-					<div>
-						<label className="clock__title">SECONDS</label>
-						<label className="clock__amount">22</label>
-					</div>
+					<label className="clock__title">SECONDS</label>
+					<label className="clock__amount">22</label>
 				</div>
 			</div>
 		);
