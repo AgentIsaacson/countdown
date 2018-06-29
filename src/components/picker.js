b64f6942c89d9c6de3b082a5c7e967e2d9de42a0
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-29 13:28:40 -0600

    cleaned up picker

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,28 +1,19 @@
 import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import moment from "moment";
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class Picker extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			startDate: moment()
-		};
-		this.handleChange = this.handleChange.bind(this);
-	}
 
-	handleChange(date) {
-        
-		this.setState({
-			startDate: date
-		});
-	}
+	handleChange = function(date) {
+		this.props.callback(date)
+	}.bind(this);
 
 	render() {
 		return (
 			<div className="picker">
 				<DatePicker
-					selected={this.state.startDate}
+					selected={this.props.startDate}
 					onChange={this.handleChange}
 				/>
 			</div>
