407361f96acd857b5b7a6b8c3796c85c8c7cd62e
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-29 14:25:41 -0600

    fixed past date calculation

--- src/components/app.js
+++ src/components/app.js
@@ -35,11 +35,32 @@ export default class App extends Component {
 	handleGenerate = function() {
 		this.setState({ active: true });
 
-		var countDownDate = this.state.startDate.toDate().getTime();
+		var bday = this.state.startDate.toDate();
+		var today = new Date();
+		var currentMonth = today.getMonth();
+		var birthMonth = bday.getMonth();
+
+		if (birthMonth > currentMonth) {
+			bday.setFullYear(today.getFullYear());
+		} else if(birthMonth < currentMonth) {
+			bday.setFullYear(today.getFullYear() + 1);
+		} else if(birthMonth == currentMonth) {
+			var currentDay = today.getDate();
+			var birthDay = bday.getDate();
+
+			if(birthDay > currentDay) {
+				bday.setFullYear(today.getFullYear());
+			}
+			if(birthDay <= currentDay) {
+				bday.setFullYear(today.getFullYear() + 1);
+			}
+		}
+
+		var countDownDate = bday.getTime();
 
 		this.timer = setInterval(
 			function() {
-				var now = new Date().getTime();
+				var now = today.getTime();
 
 				var distance = countDownDate - now;
 
@@ -63,7 +84,9 @@ export default class App extends Component {
 				if (distance < 0) {
 					clearInterval(this.timer);
 				}
-			}.bind(this), 1000);
+			}.bind(this),
+			1000
+		);
 	}.bind(this);
 
 	renderItems = function() {
