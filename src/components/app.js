a4a91ecc0d2541d545ee36a345449f434ceb83f8
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-29 10:49:04 -0600

    added and styled change date button

--- src/components/app.js
+++ src/components/app.js
@@ -2,19 +2,38 @@ import React, { Component } from "react";
 import Picker from "./picker";
 import { Button } from "./button";
 import Clock from "./clock";
+import { ChangeDate } from "./changeDate";
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			active: false
+		};
+	}
+
+	renderItems = function() {
+		if (this.state.active) {
+			return [
+				<Clock />,
+				ChangeDate('Change Date', () => this.setState({ active: false }))
+			]
+		} else {
+			return Button("Generate Countdown", () => this.setState({ active: true })) 
+		}
+
+	}.bind(this)
+
 	render() {
 		// return	<div className="grid"><Clock /></div>
-		
+
 		return (
 			<div className="grid">
 				<h1 className="grid__title">Birthday Countdown</h1>
 				<div className="grid__skew-dark-blue" />
 				<div className="grid__skew-light-blue" />
-        <Picker />
-        { Button('Generate Countdown') }
-        <Clock />
+				<Picker />
+				{this.renderItems()}
 			</div>
 		);
 	}
--- src/components/clock.js
+++ src/components/clock.js
@@ -7,25 +7,25 @@ export default class Clock extends Component {
 				<div className="clock__days clock__box">
 					<div>
 						<label className="clock__title">DAYS</label>
-						<label className="clock__amount">20</label>
+						<label className="clock__amount">300</label>
 					</div>
 				</div>
 				<div className="clock__hours clock__box">
 					<div>
 						<label className="clock__title">HOURS</label>
-						<label className="clock__amount">20</label>
+						<label className="clock__amount">16</label>
 					</div>
 				</div>
 				<div className="clock__minutes clock__box">
 					<div>
 						<label className="clock__title">MINUTES</label>
-						<label className="clock__amount">20</label>
+						<label className="clock__amount">42</label>
 					</div>
 				</div>
 				<div className="clock__seconds clock__box">
 					<div>
 						<label className="clock__title">SECONDS</label>
-						<label className="clock__amount">20</label>
+						<label className="clock__amount">22</label>
 					</div>
 				</div>
 			</div>
